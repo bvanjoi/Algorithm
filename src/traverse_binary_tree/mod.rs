97806569e4f180388af76fe1d7af14831c73b4a7
bvanjoi <610376039@qq.com>
2021-07-23 15:25:15 +0800

    feat(binary_tree): pre & in & post order

--- src/traverse_binary_tree/mod.rs
+++ src/traverse_binary_tree/mod.rs
@@ -3,28 +3,73 @@ use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 use super::binary_tree::TreeNode;
 
 /// 前序遍历
-pub fn pre_order(root: Option<Rc<RefCell<TreeNode<i32>>>>) {}
+pub fn pre_order(root: Option<Rc<RefCell<TreeNode<i32>>>>, v: &mut Vec<i32>) {
+    if root.is_none() {
+        return;
+    } else {
+        let node = root.as_ref().unwrap().borrow();
+        v.push(node.val);
+        pre_order(node.left.clone(), v);
+        pre_order(node.right.clone(), v);
+    }
+}
 
 #[test]
-fn test_pre_order() {}
+fn test_pre_order() {
+    use super::binary_tree::binary_tree_instance;
+    let r = binary_tree_instance();
+    let mut v = vec![];
+    pre_order(r, &mut v);
+    assert_eq!(v, vec![1, 2, 3, 4]);
+}
 
 /// 中序遍历
-pub fn in_order(root: Option<Rc<RefCell<TreeNode<i32>>>>) {}
+pub fn in_order(root: Option<Rc<RefCell<TreeNode<i32>>>>, v: &mut Vec<i32>) {
+    if root.is_none() {
+        return;
+    } else {
+        let node = root.as_ref().unwrap().borrow();
+        in_order(node.left.clone(), v);
+        v.push(node.val);
+        in_order(node.right.clone(), v);
+    }
+}
 
 #[test]
-fn test_in_order() {}
+fn test_in_order() {
+    use super::binary_tree::binary_tree_instance;
+    let r = binary_tree_instance();
+    let mut v = vec![];
+    in_order(r, &mut v);
+    assert_eq!(v, vec![2, 1, 4, 3]);
+}
 
 /// 后序遍历
-pub fn post_order(root: Option<Rc<RefCell<TreeNode<i32>>>>) {}
+pub fn post_order(root: Option<Rc<RefCell<TreeNode<i32>>>>, v: &mut Vec<i32>) {
+    if root.is_none() {
+        return;
+    } else {
+        let node = root.as_ref().unwrap().borrow();
+        post_order(node.left.clone(), v);
+        post_order(node.right.clone(), v);
+        v.push(node.val);
+    }
+}
 
 #[test]
-fn test_post_order() {}
+fn test_post_order() {
+    use super::binary_tree::binary_tree_instance;
+    let r = binary_tree_instance();
+    let mut v = vec![];
+    post_order(r, &mut v);
+    assert_eq!(v, vec![2, 4, 3, 1]);
+}
 
 /// 层次遍历
 pub fn level_order(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> Vec<Vec<i32>> {
     let mut q = VecDeque::new();
     let mut result = vec![];
-		
+
     if let Some(n) = root {
         q.push_back(n);
     } else {
@@ -52,8 +97,8 @@ pub fn level_order(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> Vec<Vec<i32>> {
 
 #[test]
 fn test_level_order() {
-	use super::binary_tree::binary_tree_instance;
-	let r = binary_tree_instance();
-	let v = level_order(r);
-	assert_eq!(v, vec![vec![1], vec![2,3], vec![4]]);
+    use super::binary_tree::binary_tree_instance;
+    let r = binary_tree_instance();
+    let v = level_order(r);
+    assert_eq!(v, vec![vec![1], vec![2, 3], vec![4]]);
 }
