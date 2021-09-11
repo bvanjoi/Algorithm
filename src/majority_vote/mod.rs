af83dcac4430e35f48a7f4330d5d8cc30b02e858
bvanjoi <610376039@qq.com>
2021-09-11 18:04:31 +0800

    fix(counting_init): add

--- src/majority_vote/mod.rs
+++ src/majority_vote/mod.rs
@@ -1,8 +1,10 @@
+// 多数投票算法
 pub fn majority_element(nums: Vec<i32>) -> Option<i32> {
+    // 当前元素
     let mut m: i32 = 0;
+    // 当前元素出现的次数，若其值为 0, 则将 m 切换为当前迭代的元素。
     let mut c = 0;
-    let mut index = 0;
-    while index < nums.len() {
+    for index in 0..nums.len() {
         if c == 0 {
             m = nums[index];
             c += 1;
@@ -11,8 +13,8 @@ pub fn majority_element(nums: Vec<i32>) -> Option<i32> {
         } else {
             c -= 1;
         }
-        index += 1;
     }
+    // 判断 m 出现的次数是否大于数组的一半
     let count = nums.iter().filter(|&&x| x == m).count();
     if count > nums.len() / 2 {
         Some(m)
@@ -28,4 +30,5 @@ fn test_majority_element() {
     assert_eq!(majority_element(vec![3, 2, 3]), Some(3));
     assert_eq!(majority_element(vec![2, 2, 1, 1, 1, 2, 2]), Some(2));
     assert_eq!(majority_element(vec![1, 2, 5, 9, 5, 9, 5, 5, 5]), Some(5));
+    assert_eq!(majority_element(vec![1, 2, 3, 2, 2, 2, 5, 4, 2]), Some(2));
 }
--- src/sorting/counting_sort/mod.rs
+++ src/sorting/counting_sort/mod.rs
@@ -1,23 +1,23 @@
 pub fn counting_sort(arr: Vec<i32>) -> Vec<i32> {
-  let min = *arr.iter().min().unwrap();
-  let max = *arr.iter().max().unwrap();
-  let mut c: Vec<usize> = vec![0; (max - min + 1) as usize];
-  arr.iter().for_each(|&v| c[(v - min) as usize] += 1);
-  let mut c: Vec<usize> = c
-      .iter()
-      .scan(0, |acc, &v| {
-          *acc += v;
-          Some(*acc)
-      })
-      .collect();
-  let mut b = vec![0; arr.len()];
-  arr.iter().rev().for_each(|&v| {
-      // 反向遍历 arr 中当前元素
-      // index 为该元素在 c 中的位置
-      let index = (v - min) as usize;
-      // 有 c[index] - 1 元素小于等于 v.
-      b[c[index] - 1] = v;
-      c[index] -= 1;
-  });
-  b
+    let min = *arr.iter().min().unwrap();
+    let max = *arr.iter().max().unwrap();
+    let mut c: Vec<usize> = vec![0; (max - min + 1) as usize];
+    arr.iter().for_each(|&v| c[(v - min) as usize] += 1);
+    let mut c: Vec<usize> = c
+        .iter()
+        .scan(0, |acc, &v| {
+            *acc += v;
+            Some(*acc)
+        })
+        .collect();
+    let mut b = vec![0; arr.len()];
+    arr.iter().rev().for_each(|&v| {
+        // 反向遍历 arr 中当前元素
+        // index 为该元素在 c 中的位置
+        let index = (v - min) as usize;
+        // 有 c[index] - 1 元素小于等于 v.
+        b[c[index] - 1] = v;
+        c[index] -= 1;
+    });
+    b
 }
